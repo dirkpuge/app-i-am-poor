5b067d8129c2360178c293cb1e6b865acef1d609
Dirk A Puge <dirk.puge16@gmail.com>
2020-12-15 09:32:40 -0500

    adding app project to git hub

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Dirk Puge on 2020-12-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
